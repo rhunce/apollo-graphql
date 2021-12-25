873de0e61bc12b102fcc899d3ecb2ac9643f9cc7
Raymond Hunce <hunce18c@gmail.com>
2021-12-25 11:03:08 -0500

    Complete useMutationTutorial

--- client/src/useMutationTutorial/AddTodo.js
+++ client/src/useMutationTutorial/AddTodo.js
@@ -1,17 +1,42 @@
-import { useMutation } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
 import { ADD_TODO } from '../mutations.js';
 import { GET_TODOS } from '../queries.js';
 
 export default function AddTodo() {
 	let input;
 
-	// Refetches two queries after mutation completes
 	const [addTodo, { data, loading, error }] = useMutation(ADD_TODO, {
-		refetchQueries: [GET_TODOS]
+		refetchQueries: [GET_TODOS], // If certain ToDo fields won't get updated automatically, use the update function instead to manually do so as shown below. Should also use onQueryUpdated with it to "double check" your update function's modifications by refetching affected active queries. Even more powerful, use all three.
+		update(cache, { data: { addTodo } }) {
+			cache.modify({
+				fields: {
+					todos(existingTodos = []) {
+						const newTodoRef = cache.writeFragment({
+							data: addTodo,
+							fragment: gql`
+								fragment NewTodo on Todo {
+									id
+									type
+								}
+							`,
+						});
+						return [...existingTodos, newTodoRef];
+					},
+				},
+			});
+		},
+		onQueryUpdated(observableQuery) {
+			// Define any custom logic for determining whether to refetch
+			if (shouldRefetchQuery(observableQuery)) {
+				return observableQuery.refetch();
+			}
+		},
 	});
 
-	if (loading) return 'Submitting...';
-	if (error) return `Submission error! ${error.message}`;
+  const shouldRefetchQuery = (observableQuery) => {
+    console.log('observableQuery: ', observableQuery);
+    return true;
+  };
 
 	const handleFormSubmission = (e) => {
 		e.preventDefault();
@@ -24,6 +49,9 @@ export default function AddTodo() {
 		});
 		input.value = '';
 	};
+
+  if (loading) return 'Submitting...';
+  if (error) return `Submission error! ${error.message}`;
 
 	return (
 		<div>
